f007dd21b482850ef219f334d44d20f7d5cd3ded
Andrei Dimaano <andreiidimaano@gmail.com>
2021-07-07 11:40:38 -0700

    React Course

---
+++ 12-portals-and-refs/src/App.js
@@ -0,0 +1,38 @@
+import { useState, StrictMode } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from './store'
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const theme = useState("darkblue");
+  return (
+    <Provider store={store}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
+  );
+};
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
