4aa6ce7748e993b6fd020d90d9aa718854045e5a
Andrei Dimaano <56818175+andreidimaano@users.noreply.github.com>
2022-10-14 15:43:18 -0700

    Time: 56 ms (18.79%), Space: 13.9 MB (25.81%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int ans = 0;
+        map<int, int> m;
+        
+         for(auto i: nums) {
+            m[i]++;
+        }
+        
+        for(auto i: m) {
+            if(m.find((k - i.first) * -1) != m.end()) {
+                if((k - i.first) * -1 == i.first && m[(k - i.first) * -1] == 1) continue;
+                ans++;
+                m[(k - i.first) * -1]--;
+            }
+        }
+        
+        return ans;
+    }
+};
