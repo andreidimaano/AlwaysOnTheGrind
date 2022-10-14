eb56be729db507f56b726f6f661ddc15e11e4971
Andrei Dimaano <56818175+andreidimaano@users.noreply.github.com>
2022-10-14 12:28:02 -0700

    Time: 23 ms (39.32%), Space: 10.6 MB (94.58%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -5,17 +5,20 @@ public:
         int m = mat[0].size();
         if(r * c != n * m) return mat;
         
-        queue<int> q;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                q.push(mat[i][j]);
-            }
-        }
-        
+        int row = 0, col = 0;
+        int counter = 0;
         vector<vector<int>> newMat(r, vector<int>(c, -1));
         for(int i = 0; i < r; i++) {
             for(int j = 0; j < c; j++) {
-                newMat[i][j] = q.front(); q.pop();
+                newMat[i][j] = mat[row][col];
+                counter++;
+                if(counter == m) {
+                    counter = 0;
+                    row++;
+                    col = 0;
+                } else {
+                    col++;
+                }
             }
         }
         
