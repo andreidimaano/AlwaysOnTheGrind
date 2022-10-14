c08db0418924f99ec40303d4a3a83323c1e67884
Andrei Dimaano <56818175+andreidimaano@users.noreply.github.com>
2022-10-14 12:13:58 -0700

    Time: 70 ms (51.45%), Space: 18.3 MB (69.32%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -4,23 +4,41 @@ public:
         vector<int> ans;
         int n = mat.size();
         int m = mat[0].size();
-        unordered_map<int, vector<int>> diag;
         
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                diag[i+j].push_back(mat[i][j]);
-            }
-        }
+        int row = 0;
+        int col = 0;
+        bool up = true;
         
-        for(int i = 0; i < m + n - 1; i++) {
-            if(i % 2 == 0) {
-                for(int j = diag[i].size() - 1; j >= 0; j--) {
-                    ans.push_back(diag[i][j]);
+        while(ans.size() != n * m) {
+            if(up) {
+                while(row >= 0 && col < m) {
+                    ans.push_back(mat[row][col]);
+                    row--;
+                    col++;
                 }
+                
+                if(col == m) {
+                    col--;
+                    row += 2;
+                } else {
+                    row++;
+                }
+                up = false;
             } else {
-                for(int j = 0; j < diag[i].size(); j++) {
-                    ans.push_back(diag[i][j]);
+                // going down
+                while(row < n && col >= 0) {
+                    ans.push_back(mat[row][col]);
+                    row++;
+                    col--;
                 }
+                
+                if(row == n) {
+                    row--;
+                    col += 2;
+                } else {
+                    col++;
+                }
+                up = true;
             }
         }
         
