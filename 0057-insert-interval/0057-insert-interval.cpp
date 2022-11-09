74652e6b3c4af414dd0beade566bbd4be27c7a98
Andrei Dimaano <56818175+andreidimaano@users.noreply.github.com>
2022-11-09 00:58:00 -0800

    Time: 33 ms (56.35%), Space: 17.1 MB (74.82%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -6,21 +6,21 @@ public:
         
         vector<vector<int>> result;
         
-        for(int i = 0; i < intervals.size(); i++) {
-            if(newInterval[1] < intervals[i][0]) {
-                result.push_back(newInterval);
-                for(int j = i; j < intervals.size(); j++) {
-                    result.push_back(intervals[j]);
-                }
-                return result;
-            } else if(newInterval[0] > intervals[i][1]) {
-                result.push_back(intervals[i]);
-            } else {
-                newInterval = {min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])};
-            }
+        while(i < n && intervals[i][1] < newInterval[0]) {
+            result.push_back(intervals[i++]);
+        }
+        
+        while(i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
         
         result.push_back(newInterval);
+        
+        while(i < n) {
+            result.push_back(intervals[i++]);
+        }
         
         return result;
     }
